094fe56d9eaeffcc7f0de69c7ce951f8e8d2bb86
Jed Ledermann <jledermann@students.mchenry.edu>
2024-08-26 20:16:21 -0500

    Add files via upload

---
+++ data.py
@@ -0,0 +1,22 @@
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 22
+print("Literal:", x)
+
+# Integer
+y = -33
+print("Integer:", y)
+
+# Float
+z = 92
+print("Float:", z)
+
+# String
+name = "Jed"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
