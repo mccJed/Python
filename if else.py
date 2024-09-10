a3c9646352610771ad8f67a296c63ff3748755a8
Jed Ledermann <jledermann@students.mchenry.edu>
2024-09-09 20:56:11 -0500

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,26 @@
+#age
+age= float(input("How old are you?"))
+
+#can drive?
+if age >= 16 :
+    print("You are old enough to drive")
+else:
+    print("You are not old enough to drive")
+
+#can vote?
+if age >= 18:
+    print("You are old enough to vote")
+else:
+    print("You are not old enough to vote")
+
+#can alcohol?
+if age >= 21:
+    print("You are old enough to drink")
+else:
+    print("You are not old enough to drink")
+
+#can senior?
+if age >= 65:
+    print("You are eligible for the senior discount")
+else:
+    print("You are not eligible for the senior discount")
