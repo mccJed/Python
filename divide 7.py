c254719b321bb2de7929e23f98dfd7aa52c31e87
Jed Ledermann <jledermann@students.mchenry.edu>
2024-09-09 22:43:44 -0500

    Add files via upload

---
+++ divide 7.py
@@ -0,0 +1,12 @@
+#1-300
+start = 1
+end = 300
+#divisible by 7?
+for number in range(start, end + 1):
+    # Check if the number is divisible by 7
+     if number % 7 == 0:
+         print(number)
+
+
+
+
