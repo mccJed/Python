13e6535831209c79734896373cfdd54b1ebee53c
Jed Ledermann <jledermann@students.mchenry.edu>
2024-09-23 20:22:33 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,14 @@
+#square
+def square(side):
+    area = side * side
+    print("The area of the square is " + str(area) + " square units.")
+
+# circle
+def circle(radius):
+    pi = 3.14
+    area = pi * radius * radius
+    print("The area of the circle is " + str(area) + " square units.")
+
+# Test
+square(7)  
+circle(8) 
